eba9e14c4013dc857e9223db29e4a00ef3a425ec
Uriah Harston <uharston@gmail.com>
2020-05-21 17:32:05 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -2,3 +2,10 @@ require 'nokogiri'
 require 'open-uri'
 
 html = open("https://flatironschool.com/")
+
+# doc = Nokogiri::HTML(open("https://flatironschool.com/"))
+doc = Nokogiri::HTML(html)
+
+# doc.css(".headline-26OIBN")
+doc.css(".inlineMobileLeft-2Yo002.imageTextBlockGrid2-3jXtmC")
+puts doc
